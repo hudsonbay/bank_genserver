4cc551d5395715e6df355274646d24a9dfc40c9e
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-08-11 12:45:33 -0400

    [refactor] Refactored code

--- lib/account.ex
+++ lib/account.ex
@@ -9,6 +9,7 @@ defmodule Account do
     GenServer.start_link(__MODULE__, :ok)
   end
 
+  # API
   def get_balance(pid) do
     GenServer.call(pid, :get_balance)
   end
@@ -21,30 +22,52 @@ defmodule Account do
     GenServer.cast(pid, {:withdraw, amount})
   end
 
+  # Callbacks
   def handle_call(:get_balance, _from, state) do
     {:reply, Map.get(state, :balance), state}
   end
 
+  # def handle_cast({:deposit, amount}, state) do
+  #   # balance = Map.get(state, :balance)
+  #   # {:noreply, Map.put(state, :balance, balance + amount)}
+
+  #   {_value, balance_with_deposit} =
+  #     Map.get_and_update(state, :balance, fn balance -> {balance, balance + amount} end)
+
+  #   {:noreply, balance_with_deposit}
+  # end
+
   def handle_cast({:deposit, amount}, state) do
-    # balance = Map.get(state, :balance)
-    # {:noreply, Map.put(state, :balance, balance + amount)}
-
-    {_value, balance_with_deposit} =
-      Map.get_and_update(state, :balance, fn balance -> {balance, balance + amount} end)
-
-    {:noreply, balance_with_deposit}
+    {:noreply, get_updated_balance(state, amount, &(&1 + &2))}
   end
 
-  def handle_cast({:withdraw, amount}, state) do
-    # Extracted anonymous function
-    update_balance = fn balance, amount -> balance - amount end
+  # def handle_cast({:withdraw, amount}, state) do
+  #   # We can use the & syntax, which allows us to create a smaller anonymous function,
+  #   # and reference each argument by its index &N—where N is some index.
+  #   {:noreply, change_balance(state, amount, &(&1 - &2))}
+  # end
 
+  def handle_cast({:withdraw, amount}, state) do
+    {:noreply, get_updated_balance(state, amount, &(&1 - &2))}
+  end
+
+  # extracted function
+  # defp change_balance(state, amount, calculate_balance) do
+  #   {_value, updated_balance} =
+  #     Map.get_and_update(state, :balance, fn balance ->
+  #       {balance, calculate_balance.(balance, amount)}
+  #     end)
+
+  #   updated_balance
+  # end
+
+  # private
+  defp get_updated_balance(state, amount, calculate_balance) do
     {_value, updated_balance} =
       Map.get_and_update(state, :balance, fn balance ->
-        # calling the function instead of doing a calculation
-        {balance, update_balance.(balance, amount)}
+        {balance, calculate_balance.(balance, amount)}
       end)
 
-    {:noreply, updated_balance}
+    updated_balance
   end
 end
